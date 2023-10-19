9df1fee5e27934865e4867ac5d2521de991ab7c6
marcio sieto <mmmsieto@hotmail.com>
2023-10-18 22:33:21 -0300

    update

---
+++ server/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type ApiResponse struct {
+	USDBRL USDBRL `json:"USDBRL"`
+}
+
+type USDBRL struct {
+	Code        string `json:"code"`
+	Codein      string `json:"codein"`
+	Name        string `json:"name"`
+	High        string `json:"high"`
+	Low         string `json:"low"`
+	VarBid      string `json:"varBid"`
+	PctChange   string `json:"pctChange"`
+	Bid         string `json:"bid"`
+	Ask         string `json:"ask"`
+	Timestamp   string `json:"timestamp"`
+	Create_date string `json:"create_date"`
+}
+
+func BuscaCotacaoUSDBRL(w http.ResponseWriter, r *http.Request) {
+
+	response, err := BuscaCotacao()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	createFile(err, response)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
+func createFile(err error, response *ApiResponse) {
+	file, err := os.Create("cotacao.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erro ao criar arquivo: %v\n", err)
+	}
+	defer file.Close()
+	_, err = file.WriteString(fmt.Sprintf("Dólar: %s\n", response.USDBRL.Bid))
+}
+
+func BuscaCotacao() (*ApiResponse, error) {
+	req, err := http.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+	if err != nil {
+		return nil, err
+	}
+	defer req.Body.Close()
+	res, err := io.ReadAll(req.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var response ApiResponse
+	err = json.Unmarshal(res, &response)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &response, nil
+}
+
+func main() {
+	http.HandleFunc("/cotacao", BuscaCotacaoUSDBRL)
+	fmt.Println("Server running on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
