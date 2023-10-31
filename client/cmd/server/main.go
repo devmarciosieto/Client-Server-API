c09f95468931ee9f24ec6bcbd5d4a500e34a7de5
marcio sieto <mmmsieto@hotmail.com>
2023-10-30 21:33:35 -0300

    update

--- client/cmd/server/main.go
+++ client/cmd/server/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"github.com/devmarciosieto/Client-Server-API/client/handlers"
 	"log"
 	"net/http"
 )
 
 func main() {
+	fileServer := http.FileServer(http.Dir("../../public"))
 	mux := http.NewServeMux()
+	mux.Handle("/", fileServer)
 	mux.HandleFunc("/cotacao", handlers.BuscaCotacaoUSDBRL)
+	fmt.Println("Server running on port 8081")
 	log.Fatal(http.ListenAndServe(":8081", mux))
 }
